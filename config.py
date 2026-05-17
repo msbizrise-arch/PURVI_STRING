9a2ad9a178361589267354ce5b261170b7ebdb76
msbizrise-arch <msbizrise@gmail.com>
2026-05-17 17:25:37 +0530

    Change default BOT_TOKEN value to an empty string

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "38498066"))
 API_HASH = getenv("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", " ")
 OWNER_ID = int(getenv("OWNER_ID", "8703802029"))
 
 MONGO_DB_URI = getenv("mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
